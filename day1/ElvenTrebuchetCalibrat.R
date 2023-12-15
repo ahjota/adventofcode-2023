d4ca26fa6275862ea3ea712a9db11b75eba05ccd
AJ Alon <alexander.joel.alon@gmail.com>
2023-12-14 23:59:01 -0800

    rewrite as a better function

--- day1/ElvenTrebuchetCalibrat.R
+++ day1/ElvenTrebuchetCalibrat.R
@@ -1,6 +1,15 @@
 library(stringr)
 
-recoverCalibrationValue <- function(x) x[1] * 10 + x[length(x)]
+recoverCalibrationValue <- function(input) {
+  f <- function(x) x[1] * 10 + x[length(x)]
+  
+  # assume input is a character vector
+  stringr::str_extract_all(input, "[0-9]") %>% 
+    sapply(., as.numeric) %>% 
+    sapply (., f)
+}
+
+
 
 input <- read.csv2("day1/input.txt", header = FALSE, blank.lines.skip = TRUE) %>%
     .$V1 # %>% head
@@ -10,7 +19,4 @@ input <- read.csv2("day1/input.txt", header = FALSE, blank.lines.skip = TRUE) %>
 # 
 # sumCalibrationValues <- sum(calibrationValues)
 
-sumCalibrationValues <- stringr::str_extract_all(input, "[0-9]") %>% 
-  sapply(., as.numeric) %>% 
-  sapply(., recoverCalibrationValue) %>% 
-  sum
+sumCalibrationValues <- sum(recoverCalibrationValue(input))
