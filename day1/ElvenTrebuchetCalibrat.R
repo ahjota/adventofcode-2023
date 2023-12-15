409aa76c5a2814eb68602e63b8d689ae6e51bd6d
AJ Alon <alexander.joel.alon@gmail.com>
2023-12-14 23:55:09 -0800

    refactored with pipes

--- day1/ElvenTrebuchetCalibrat.R
+++ day1/ElvenTrebuchetCalibrat.R
@@ -1,9 +1,16 @@
 library(stringr)
 
-input <- read.csv2("day1/input.txt", header = FALSE, blank.lines.skip = TRUE)
-input <- head(input$V1)
-digits <- sapply(stringr::str_extract_all(input, "[0-9]"), as.numeric)
 recoverCalibrationValue <- function(x) x[1] * 10 + x[length(x)]
-calibrationValues <- sapply(digits, recoverCalibrationValue)
 
-sumCalibrationValues <- sum(calibrationValues)
+input <- read.csv2("day1/input.txt", header = FALSE, blank.lines.skip = TRUE) %>%
+    .$V1 # %>% head
+# 
+# digits <- sapply(stringr::str_extract_all(input, "[0-9]"), as.numeric)
+# calibrationValues <- sapply(digits, recoverCalibrationValue)
+# 
+# sumCalibrationValues <- sum(calibrationValues)
+
+sumCalibrationValues <- stringr::str_extract_all(input, "[0-9]") %>% 
+  sapply(., as.numeric) %>% 
+  sapply(., recoverCalibrationValue) %>% 
+  sum
