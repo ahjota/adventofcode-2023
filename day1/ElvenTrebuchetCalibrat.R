f311c9cb5a8a2d8e4d2891d0b36d39bb5a45bcf4
AJ Alon <alexander.joel.alon@gmail.com>
2023-12-15 00:26:26 -0800

    Day 1 Part 2 potential solution

--- day1/ElvenTrebuchetCalibrat.R
+++ day1/ElvenTrebuchetCalibrat.R
@@ -1,15 +1,31 @@
 library(stringr)
 
+digitSpellings <- c(
+  "one" = "1",
+  "two" = "2",
+  "three" = "3",
+  "four" = "4",
+  "five" = "5",
+  "six" = "6",
+  "seven" = "7",
+  "eight" = "8",
+  "nine" = "9"
+)
+
+extractCalibrationValueCandidates <- function(input) {
+  candidates <- stringr::str_extract_all(input, "[0-9]|one|two|three|four|five|six|seven|eight|nine")
+  sapply(candidates, function(x) stringr::str_replace_all(x, digitSpellings))
+}
+
 recoverCalibrationValue <- function(input) {
   f <- function(x) x[1] * 10 + x[length(x)]
   
   # assume input is a character vector
-  stringr::str_extract_all(input, "[0-9]") %>% 
+  input %>%
+    extractCalibrationValueCandidates %>% 
     sapply(., as.numeric) %>% 
     sapply (., f)
 }
-
-
 
 input <- read.csv2("day1/input.txt", header = FALSE, blank.lines.skip = TRUE) %>%
     .$V1 # %>% head
@@ -19,4 +35,6 @@ input <- read.csv2("day1/input.txt", header = FALSE, blank.lines.skip = TRUE) %>
 # 
 # sumCalibrationValues <- sum(calibrationValues)
 
-sumCalibrationValues <- sum(recoverCalibrationValue(input))
+sumCalibrationValues <- input %>%
+  recoverCalibrationValue %>% 
+  sum
